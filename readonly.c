d42c2d7221de63e61cd1b31dc32af3475b557191
gunjikar <gunjikar.kn@gmail.com>
2013-10-24 21:39:48 -0700

    Update readonly.c

--- readonly.c
+++ readonly.c
@@ -4,9 +4,14 @@ main()
 {
   char str[] = "Hello World!";
   char *f =" This is the string";
-   
+  char *first = "Times";
+  char *second = "Times";
+  
   *(str +2) = 'x';
   
   *(f + 1) = 'x';
   printf("%s \n %s", str, f);
+  
+  printf("\n %p %p",first, second);
+  
 }
